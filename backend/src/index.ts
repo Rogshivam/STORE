b95c6a15fc9f0716b3cbf277ef9b720912922f7d
Rogshivam <shivamk2000.com@gmail.com>
2026-01-12 00:43:55 +0530

    setup  frontendd and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import * as express from "express";
+import { ENV } from "./config/env";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "welcome to productivity API - powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments"
+    }
+  });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is up and running on PORT:", ENV.PORT);
+});
